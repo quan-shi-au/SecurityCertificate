73668c38762cbcd9e5668d843ea157505b78df58
DEV-SAM\Sam Quan <sam.quan@wontok.com>
2018-04-13 17:14:16 +1000

    no message

---
+++ ClientCaller/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientCaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                X509Certificate2 clientCert = GetClientCertificate();
+                WebRequestHandler requestHandler = new WebRequestHandler();
+                requestHandler.ClientCertificates.Add(clientCert);
+
+                HttpClient client = new HttpClient(requestHandler)
+                {
+                    BaseAddress = new Uri("http://localhost:3020/")
+                };
+
+                HttpResponseMessage response = client.GetAsync("customers").Result;
+                response.EnsureSuccessStatusCode();
+                string responseContent = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(responseContent);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception while executing the test code: {0}", ex.Message);
+            }
+            Console.ReadKey();
+        }
+
+        private static X509Certificate2 GetClientCertificate()
+        {
+            X509Store userCaStore = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            try
+            {
+                userCaStore.Open(OpenFlags.ReadOnly);
+                X509Certificate2Collection certificatesInStore = userCaStore.Certificates;
+                X509Certificate2Collection findResult = certificatesInStore.Find(X509FindType.FindBySubjectName, "localtestclientcert", true);
+                X509Certificate2 clientCertificate = null;
+                if (findResult.Count == 1)
+                {
+                    clientCertificate = findResult[0];
+                }
+                else
+                {
+                    throw new Exception("Unable to locate the correct client certificate.");
+                }
+                return clientCertificate;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                userCaStore.Close();
+            }
+        }
+
+    }
+
+}
---
+++ ClientCertTests/Controllers/CustomersController.cs
@@ -0,0 +1,31 @@
+﻿using ClientCertTests.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Web.Http;
+
+namespace ClientCertTests.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            X509Certificate2 clientCertInRequest = RequestContext.ClientCertificate;
+            //X509Certificate2 clientCertInRequest = Request.GetClientCertificate();
+            if (clientCertInRequest != null)
+            {
+                IList<Customer> customers = new List<Customer>();
+                customers.Add(new Customer() { Name = "Nice customer", Address = "USA", Telephone = "123345456" });
+                customers.Add(new Customer() { Name = "Good customer", Address = "UK", Telephone = "9878757654" });
+                customers.Add(new Customer() { Name = "Awesome customer", Address = "France", Telephone = "34546456" });
+                return Ok<IList<Customer>>(customers);
+            }
+            AuthenticationHeaderValue authHeaderValue = new AuthenticationHeaderValue("ClientCert");
+            return Unauthorized(authHeaderValue);
+        }
+    }
+}
